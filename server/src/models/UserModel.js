098eefc2ea778a8c911255bb712ba068e8bdc738
muhammetsarican <muhammetsarican@gmail.com>
2024-07-29 11:01:08 +0300

    feat: User model created.

---
+++ server/src/models/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    mail: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        select: false
+    },
+    role: String,
+    status: String
+}, {
+    versionKey: false,
+    timestamps: true
+})
+
+module.exports = mongoose.model("user", UserSchema);
