fd2c740557fe1468459d729279a602dee38f86ae
muhammetsarican <muhammetsarican@gmail.com>
2024-07-29 11:56:59 +0300

    feat(Animal model): Animal model created, refactor(User model): mongoose import adjusted.

---
+++ server/src/models/AnimalModel.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+const AnimalSchema = new mongoose.Schema({
+    mother_id: {
+        type: mongoose.Schema.ObjectId,
+        ref: "animal"
+    },
+    genre_id: {
+        type: mongoose.Schema.ObjectId,
+        ref: "genre"
+    },
+    chip_status: String,
+    name: String,
+    gender: String,
+    born_date: Date,
+    vaccines: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "vaccine"
+    }],
+    status: String
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+module.exports = mongoose.model("animal", AnimalSchema);
--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     fname: String,
