b1915d3998aa3d0ce3e4278d6503ac809e8a1083
muhammetsarican <muhammetsarican@gmail.com>
2024-07-30 13:38:43 +0300

    feat(user controller): User controller created, feat(hash password): New helper created for hashing passwords.

---
+++ server/src/controllers/UserController.js
@@ -0,0 +1,43 @@
+const BaseController = require("./BaseController");
+const UserService = require("../services/UserService");
+const SuccessMessage = require("../handlers/SuccessMessage");
+const HashPassword = require("../helpers/HashPassword");
+
+class UserController extends BaseController {
+    constructor() {
+        super(UserService);
+    }
+
+    login() {
+        return (req, res, next) => {
+            req.body.password = HashPassword(req.body.password);
+            UserService.read(req.body)
+                .then(response => {
+                    req.user = response;
+                    res.status(200).send(new SuccessMessage(response));
+                })
+        }
+    }
+
+    register() {
+        return (req, res, next) => {
+            req.body.password = HashPassword(req.body.password);
+            UserService.create(req.body)
+                .then(response => {
+                    req.user = response;
+                    res.status(201).send(new SuccessMessage(response));
+                })
+        }
+    }
+
+    changeRole() {
+        return (req, res, next) => {
+            UserService.update(req.params.user_id, req.body.role)
+                .then(response => {
+                    res.status(200).send(new SuccessMessage(response));
+                })
+        }
+    }
+}
+
+module.exports = new UserController();
---
+++ server/src/helpers/HashPassword.js
@@ -0,0 +1,7 @@
+const HmacSHA512 = require("crypto-js/hmac-sha512");
+
+const HashPassword = (password) => {
+    return HmacSHA512(password, process.env.PASSWORD_KEY);
+}
+
+module.exports = HashPassword;
