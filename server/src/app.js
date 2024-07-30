27096886d812d40c526e5aa441344aad9d84e909
muhammetsarican <muhammetsarican@gmail.com>
2024-07-30 18:08:04 +0300

    feat: Database conf func imported and express json added.

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const config = require("./config");
 
 const { clo } = require("./utils/CustomConsoleLog");
+const { database } = require("./database");
 
 config();
+database();
 
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.APP_PORT || "3000";
 
