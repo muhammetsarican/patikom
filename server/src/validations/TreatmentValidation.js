5f85a14019809a407d28a109d23cdef53dfcd779
muhammetsarican <muhammetsarican@gmail.com>
2024-07-31 16:55:19 +0300

    feat: Treatment validation created.

---
+++ server/src/validations/TreatmentValidation.js
@@ -0,0 +1,36 @@
+const yup = require("yup");
+
+const createValidation = yup.object({
+    title: yup.string(),
+    keyword: yup.string(),
+    description: yup.string(),
+    categories: yup.array().default([]),
+    medicines: yup.array().default([])
+})
+
+const updateValidation = yup.object({
+    title: yup.string(),
+    keyword: yup.string(),
+    description: yup.string(),
+    categories: yup.array().default([]),
+    medicines: yup.array().default([])
+})
+
+const addCategoryValidation = yup.object({
+    title: yup.string().required(),
+    keyword: yup.string(),
+    description: yup.string()
+})
+
+const addMedicineValidation = yup.object({
+    title: yup.string().required(),
+    keyword: yup.string(),
+    description: yup.string()
+})
+
+module.exports.schemas = {
+    createValidation,
+    updateValidation,
+    addCategoryValidation,
+    addMedicineValidation
+}
