82763c11e9ab941b777b688858399578e7002aa4
muhammetsarican <muhammetsarican@gmail.com>
2024-07-31 16:20:45 +0300

    feat: Folk validation created.

---
+++ server/src/validations/FolkValidation.js
@@ -0,0 +1,18 @@
+const yup = require("yup");
+
+const createValidation = yup.object({
+    genre_id: yup.string().required(),
+    name: yup.string().required(),
+    country: yup.string()
+})
+
+const updateValidation = yup.object({
+    genre_id: yup.string(),
+    name: yup.string(),
+    country: yup.string()
+})
+
+module.exports.schemas = {
+    createValidation,
+    updateValidation
+}
