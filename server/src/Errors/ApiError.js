456edc751203bf1df505744d2daaa6dea0175893
muhammetsarican <muhammetsarican@gmail.com>
2024-07-31 13:03:27 +0300

    refactor: Api error structure changed for better error handling.

--- server/src/Errors/ApiError.js
+++ server/src/Errors/ApiError.js
@@ -1,10 +1,26 @@
 const ErrorMessage = require("../handlers/ErrorMessage");
 
 class ApiError extends Error {
-    constructor(message, statusCode) {
-        super(new ErrorMessage(message));
+    set(message, statusCode) {
+        super.message = new ErrorMessage(message);
         this.status = statusCode;
+    }
+    badRequest() {
+        super.message = new ErrorMessage("Bad request!");
+        this.status = 400;
+    }
+    unauthorized() {
+        super.message = new ErrorMessage("Not authorized!");
+        this.status = 401;
+    }
+    forbidden() {
+        super.message = new ErrorMessage("Forbidded!");
+        this.status = 403;
+    }
+    notFound() {
+        super.message = new ErrorMessage("No records found!");
+        this.status = 404;
     }
 }
 
-module.exports = ApiError;
+module.exports = new ApiError();
