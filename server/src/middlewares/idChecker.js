a6d4449f5aac678da477e69dcbd796fb79a722f4
muhammetsarican <muhammetsarican@gmail.com>
2024-07-31 23:34:08 +0300

    fix: I forgot > sign near of the field.

--- server/src/middlewares/idChecker.js
+++ server/src/middlewares/idChecker.js
@@ -1,6 +1,6 @@
 const ApiError = require("../Errors/ApiError");
 
-module.exports = (field) = (req, res, next) => {
+module.exports = (field) => (req, res, next) => {
     if (!req?.params[field || "id"]?.match(/^[0-9a-fA-F]{24}$/)) return next(ApiError.unvalidId());
     next();
 }
