d9730d7c3057800af6116ff031ea4e97995528a4
muhammetsarican <muhammetsarican@gmail.com>
2024-08-01 17:28:01 +0300

    feat: all routes added to index route.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
-const { UserRoute } = require("./UserRoute");
+const UserRoute = require("./UserRoute");
+const ChipRoute = require("./ChipRoute");
+const AnimalRoute = require("./AnimalRoute");
+const FolkRoute = require("./FolkRoute");
+const GenreRoute = require("./GenreRoute");
+const PregnantLogRoute = require("./PregnantLogRoute");
+const TreatmentRoute = require("./TreatmentRoute");
+const CategoryRoute = require("./CategoryRoute");
+const MedicineRoute = require("./MedicineRoute");
+const VaccineRoute = require("./VaccineRoute");
 
 const app = express();
 
@@ -10,6 +19,16 @@ app.get("/", (req, res, next) => {
     })
 })
 
+app.use("/user", UserRoute.indexRoutes());
+app.use("/chip", ChipRoute.indexRoutes());
+app.use("/animal", AnimalRoute.indexRoutes());
+app.use("/folk", FolkRoute.indexRoutes());
+app.use("/genre", GenreRoute.indexRoutes());
+app.use("/pregnant-log", PregnantLogRoute.indexRoutes());
+app.use("/treatment", TreatmentRoute.indexRoutes());
+app.use("/category", CategoryRoute.indexRoutes());
+app.use("/medicine", MedicineRoute.indexRoutes());
+app.use("/vaccine", VaccineRoute.indexRoutes());
 app.use("/user", UserRoute.indexRoutes());
 
 module.exports.BaseRoutes = app
