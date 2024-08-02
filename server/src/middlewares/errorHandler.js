ac4208f9861ae582d9eb998331d22ef1a1abee32
muhammetsarican <muhammetsarican@gmail.com>
2024-08-02 23:43:35 +0300

    refactor: unnecessary libs removed.

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,8 +1,6 @@
-const ApiError = require("../errors/ApiError");
-const ErrorMessage = require("../handlers/ErrorMessage");
 const { clo } = require("../utils/CustomConsoleLog")
 
 module.exports = (error, req, res, next) => {
-    clo.g(error);
+    clo.g(error.message.message);
     res.status(error.status || 500).send(error.message || "Internal server error!");
 }
