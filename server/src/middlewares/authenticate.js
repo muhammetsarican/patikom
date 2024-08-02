d23f655316ef72fe2d97a227ce6917f62582fb72
muhammetsarican <muhammetsarican@gmail.com>
2024-08-02 23:47:43 +0300

    feat: json web token add for authenticate the user.

--- server/src/middlewares/authenticate.js
+++ server/src/middlewares/authenticate.js
@@ -1,14 +1,22 @@
-const ApiError = require("../errors/ApiError")
+const jwt = require("jsonwebtoken");
+
+const ApiError = require("../errors/ApiError");
+const UserService = require("../services/UserService");
 
 module.exports = (req, res, next) => {
-    if (!req.user) return next(ApiError.notAuthenticated());
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1];
 
-    UserService.read(req.user)
-        .then(() => {
-            next();
-        })
-        .catch(err => {
-            req.user = null;
-            next(ApiError.notAuthenticated());
-        })
+    if (!token) next(ApiError.notAuthenticated());
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
+        if (err) return next(ApiError.set(err, 403));
+
+        UserService.readOne({ _id: user._id })
+            .then(response => {
+                if (!response) return next(ApiError.noMatchToken());
+                req.user = response;
+                next();
+            })
+    })
 }
