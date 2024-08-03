3a9d7d600066d97b455ecf39bf7383164cce7e50
muhammetsarican <muhammetsarican@gmail.com>
2024-08-03 18:31:12 +0300

    feat: helmet and cors libs added to project.

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
 const config = require("./config");
+const { database } = require("./database");
 
 const { clo } = require("./utils/CustomConsoleLog");
-const { database } = require("./database");
 
 const ApiError = require("./errors/ApiError");
 const errorHandler = require("./middlewares/errorHandler");
@@ -14,6 +17,8 @@ database();
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 
 const PORT = process.env.APP_PORT || "3000";
 
@@ -23,7 +28,7 @@ app.listen(PORT, () => {
     app.use("/", BaseRoutes);
 
     app.use((req, res, next) => {
-        next(ApiError.set("The page not found", 404));
+        return next(ApiError.set("The page not found", 404));
     })
 
     app.use(errorHandler);
