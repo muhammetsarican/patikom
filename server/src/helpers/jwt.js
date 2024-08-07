1432324d210e147d4deea998e7e54c6eb8279a26
muhammetsarican <muhammetsarican@gmail.com>
2024-08-07 18:07:42 +0300

    refactor: user objects converting to json at user controller now, and new error handlers added.

--- server/src/helpers/jwt.js
+++ server/src/helpers/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 const createAccessToken = (user) => {
-    return jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN_SECRET_KEY, { algorithm: 'HS512', expiresIn: "120m" });
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, { algorithm: 'HS512', expiresIn: "120m" });
 }
 
 const createRefreshToken = (user) => {
-    return jwt.sign(user.toJSON(), process.env.REFRESH_TOKEN_SECRET_KEY, { algorithm: 'HS512', expiresIn: "120m" });
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET_KEY, { algorithm: 'HS512', expiresIn: "120m" });
 }
 
 module.exports = {
