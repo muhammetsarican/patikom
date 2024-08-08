7bc9ef7bb28384816a2788b729fccb4934645286
muhammetsarican <muhammetsarican@gmail.com>
2024-08-08 23:49:10 +0300

    feat: vaccine route test created.

---
+++ server/src/__tests__/routes/VaccineRoute.test.js
@@ -0,0 +1,143 @@
+const request = require("supertest");
+const app = require("../../app.js");
+const { default: mongoose } = require("mongoose");
+const customExpect = require("../../../.jest/CustomExpect.js");
+const createTestUser = require("../../../.jest/CreateTestData/CreateTestUser.js");
+const createTestVaccine = require("../../../.jest/CreateTestData/CreateTestVaccine.js");
+
+let vaccine, users;
+
+describe("/vaccine", () => {
+    beforeAll(async () => {
+        await createTestUser.init();
+        await createTestVaccine.init();
+
+        users = await createTestUser.saveTestUsers();
+
+        vaccine = await createTestVaccine.saveTestVaccine();
+    })
+
+    afterAll(() => {
+        mongoose.connection.close();
+    })
+
+    describe("GET", () => {
+        describe("/", () => {
+            test("without token", async () => {
+                const res = await request(app).get("/vaccine")
+
+                customExpect(res).toBeForbidden();
+            })
+            test("with unauthorized token", async () => {
+                const res = await request(app).get("/vaccine").set("authorization", `Bearer ${users.default.tokens.access_token}`)
+
+                customExpect(res).toBeUnauthorized();
+            })
+            test("with authorized token", async () => {
+                const res = await request(app).get("/vaccine").set("authorization", `Bearer ${users.admin.tokens.access_token}`)
+
+                customExpect(res).toBeOk();
+            })
+        })
+        describe("/:id", () => {
+            test("without token", async () => {
+                const res = await request(app).get(`/vaccine/${vaccine._id}`);
+
+                customExpect(res).toBeForbidden();
+            })
+            test("with invalid id", async () => {
+                const res = await request(app).get(`/vaccine/${vaccine._id}z`).set("authorization", `Bearer ${users.default.tokens.access_token}`);;
+
+                customExpect(res).toBeBadRequest();
+            })
+            test("if vaccine not exist", async () => {
+                const res = await request(app).get(`/vaccine/${new mongoose.Types.ObjectId()}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);;
+
+                customExpect(res).toBeNotFound();
+            })
+            test("if all correct", async () => {
+                const res = await request(app).get(`/vaccine/${vaccine._id}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);;
+
+                customExpect(res).toBeOk();
+            })
+        })
+    })
+    describe("POST", () => {
+        describe("/", () => {
+            test("without token", async () => {
+                const res = await request(app).post(`/vaccine`).send({
+                });
+
+                customExpect(res).toBeForbidden();
+            })
+            test("with no info", async () => {
+                const res = await request(app).post(`/vaccine`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeBadRequest();
+            })
+            test("if all correct", async () => {
+                const res = await request(app).post(`/vaccine`).send({
+                    name: "wear earring"
+                }).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeCreated();
+            })
+        })
+    })
+    describe("PATCH", () => {
+        describe("/:id", () => {
+            test("with no token", async () => {
+                const res = await request(app).patch(`/vaccine/${vaccine._id}`);
+
+                customExpect(res).toBeForbidden();
+            })
+            test("with invalid id", async () => {
+                const res = await request(app).patch(`/vaccine/${vaccine._id}z`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeBadRequest()
+            })
+            test("with not recorded id", async () => {
+                const res = await request(app).patch(`/vaccine/${new mongoose.Types.ObjectId()}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeNotFound()
+            })
+            test("with no info", async () => {
+                const res = await request(app).patch(`/vaccine/${vaccine._id}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeOk()
+            })
+            test("if all correct", async () => {
+                const res = await request(app).patch(`/vaccine/${vaccine._id}`).send({
+                    name: "wear earring"
+                })
+                    .set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeOk()
+            })
+        })
+    })
+    describe("DELETE", () => {
+        describe("/:id", () => {
+            test("with no token", async () => {
+                const res = await request(app).delete(`/vaccine/${vaccine._id}`);
+
+                customExpect(res).toBeForbidden();
+            })
+            test("with invalid id", async () => {
+                const res = await request(app).delete(`/vaccine/${vaccine._id}z`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeBadRequest()
+            })
+            test("with not recorded id", async () => {
+                const res = await request(app).delete(`/vaccine/${new mongoose.Types.ObjectId()}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeNotFound()
+            })
+            test("all things are correct", async () => {
+                const res = await request(app).delete(`/vaccine/${vaccine._id}`).set("authorization", `Bearer ${users.default.tokens.access_token}`);
+
+                customExpect(res).toBeOk()
+            })
+        })
+    })
+})
