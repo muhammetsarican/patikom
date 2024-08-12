21faf0fbf7c87ba6527d70c5a4ba1c7b80718d59
muhammetsarican <muhammetsarican@gmail.com>
2024-08-12 22:18:58 +0300

    feat: new fonts and new colors imported, container field features added.

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -1,10 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**.html"
+    "./src/pages/*.html"
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
+    extend: {
+      colors: {
+        primary: "#FFA500",
+        accent: "#FFD700",
+        background: "#DCDCDC",
+        secondary: "#F08080",
+        muted: "#C0C0C0",
+        "primary-text": "#333333",
+        "secondary-text": "#6A5ACD",
+        "accent-text": "#FF8C00",
+        "muted-text": "#808080",
+        "link-text": "#1E90FF",
+        "off-white-text": "#F8F8F8",
+        "ivory-text": "#FFFFF0",
+      },
+      fontFamily: {
+        tiny5: ["Tiny5", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        kanit: ["Kanit", "sans-serif"],
+        sora: ["Sora", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
