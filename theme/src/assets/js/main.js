6351104c638c33710ad9d0bb5f5f64548394475f
muhammetsarican <muhammetsarican@gmail.com>
2024-08-15 00:06:32 +0300

    refactor: main js file seperated two file because of it not works with special codes, not finds its references and returns errors.

--- theme/src/assets/js/main.js
+++ theme/src/assets/js/main.js
@@ -1,46 +1,10 @@
-const newsContainer = document.querySelector("#news-container");
-const newsItems = document.querySelectorAll("#news-item");
+const avatarButton = document.querySelector("#avatar");
+const avatarBox = document.querySelector("#avatar-box");
 
-const prevButton = document.querySelector("#prev");
-const nextButton = document.querySelector("#next");
-
-let leftCounter = 0;
-
-const newsItemWidth = newsItems[0].getBoundingClientRect().width;
-const containerWidth = newsContainer.getBoundingClientRect().width - newsItemWidth;
-
-newsContainer.style.left = "0px";
-
-const slipLeft = () => {
-    if (leftCounter > 0) {
-        leftCounter -= newsItemWidth;
-        newsContainer.style.left = `-${leftCounter}px`
-    }
-}
-
-const slipRight = () => {
-    if (leftCounter == containerWidth) resetSlider();
-    else if (leftCounter < containerWidth) {
-        leftCounter += newsItemWidth;
-        newsContainer.style.left = `-${leftCounter}px`
-    }
-}
-
-const resetSlider = () => {
-    leftCounter = 0;
-    newsContainer.style.left = `${leftCounter}px`
-}
-
-const autoSlip = setInterval(() => {
-    slipRight();
-}, 5000)
-
-prevButton.addEventListener("click", () => {
-    clearInterval(autoSlip);
-    slipLeft()
+// avatar button - script
+avatarButton.addEventListener("click", () => {
+    avatarBox.classList.toggle("hidden");
+    avatarButton.classList.toggle("bg-primary");
+    avatarButton.classList.toggle("bg-tertiary");
 })
-
-nextButton.addEventListener("click", () => {
-    clearInterval(autoSlip);
-    slipRight()
-})
+// avatar button - script
