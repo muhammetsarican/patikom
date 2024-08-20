55a6cd46594d78b1fc347cc213ba6f2511522396
muhammetsarican <muhammetsarican@gmail.com>
2024-08-20 22:43:44 +0300

    feat: new icons added to app.

---
+++ client/src/assets/icons/moon-star.tsx
@@ -0,0 +1,3 @@
+export default ({ className }: { className: string | null }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#C0C0C0" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className={`lucide lucide-moon-star ${className}`}><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9" /><path d="M20 3v4" /><path d="M22 5h-4" /></svg>
+)
---
+++ client/src/assets/icons/social-media/instagram.tsx
@@ -0,0 +1,3 @@
+export default ({ className }: { className: string | null }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#C0C0C0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide lucide-instagram ${className}`}><rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg>
+)
